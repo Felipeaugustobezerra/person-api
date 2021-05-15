565bab84c45bbea906e3f9f12c5658298080678d
phototype2 <58082993+phototype2@users.noreply.github.com>
2021-05-15 00:14:17 -0300

    Developed unit test for create person

---
+++ src/test/java/one/innovation/digital/Person/API/service/PersonServiceTest.java
@@ -0,0 +1,51 @@
+package one.innovation.digital.Person.API.service;
+
+
+import one.innovation.digital.Person.dto.request.PersonDTO;
+import one.innovation.digital.Person.dto.response.MessageResponseDTO;
+import one.innovation.digital.Person.entity.Person;
+import one.innovation.digital.Person.repository.PersonRepository;
+import one.innovation.digital.Person.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import static one.innovation.digital.Person.API.utils.PersonUtils.createFakeDTO;
+import static one.innovation.digital.Person.API.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+        MessageResponseDTO succesMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, succesMessage);
+    }
+
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
---
+++ src/test/java/one/innovation/digital/Person/API/utils/PhoneUtils.java
@@ -0,0 +1,27 @@
+package one.innovation.digital.Person.API.utils;
+
+import one.digitalinnovation.personapi.enums.PhoneType;
+import one.innovation.digital.Person.dto.request.PhoneDTO;
+import one.innovation.digital.Person.entity.Phone;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
